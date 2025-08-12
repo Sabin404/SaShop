eb8fc8525a0091aa16e9c75ebea1ebf4ad317321
Sabin <paudelsabin0@gmail.com>
2025-08-12 22:19:41 +0545

    address Ui

--- client/src/components/shopping-view/Address.jsx
+++ client/src/components/shopping-view/Address.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Card, CardContent, CardHeader, CardTitle } from '../ui/card'
+import Form from '../common/Form'
+import { addressFormControls } from '@/config'
+
+const initialData={
+  address:'',
+  city:'',
+  phone:'',
+  pincode:'',
+  notes:''
+}
 
 const Address = () => {
+  const [formData,setFormData]=useState(initialData)
+
+  const handleManageAddress=(e)=>{
+    e.preventDefault();
+  }
+
+  function isValid(){
+    return Object.keys(formData).map(key=>formData[key].trim()!=='').every(item=>item)
+  }
+
   return (
-    <div>
-      Address COmpo
-    </div>
+    <Card>
+      <div>Address</div>
+      <CardHeader>
+        <CardTitle>Add New Address</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Form
+          formControls={addressFormControls}
+          formData={formData}
+          setFormData={setFormData}
+          buttonText={'Add'}
+          onSubmit={handleManageAddress}
+          isBtnDisabled={!isValid()}
+        />
+      </CardContent>
+    </Card>
   )
 }
 
